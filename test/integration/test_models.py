e56f5f2932439145da30f96e2716693bdc52e039
allyboy08 <alexsassman65@gmail.com>
2021-05-19 16:18:59 +0200

    changed

--- test/integration/test_models.py
+++ test/integration/test_models.py
@@ -8,7 +8,7 @@ from website.__init__ import db
 class TestModels(BaseTest):
     def test_crud(self):
         with self.app_context():
-            note = Note(data='test1', date=17 / 5 / 2021, user_id=1)
+            note = Note(data='test', date=17 / 5 / 2021, user_id=1)
             
             db.session.query(Note).filter_by(data="test").first()
             
