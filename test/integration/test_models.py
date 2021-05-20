f37d9d43830a78e3debb4b4b79d2833238d7ede7
allyboy08 <alexsassman65@gmail.com>
2021-05-20 16:13:38 +0200

    updated

--- test/integration/test_models.py
+++ test/integration/test_models.py
@@ -8,32 +8,48 @@ from website.__init__ import db
 class TestModels(BaseTest):
     def test_crud(self):
         with self.app_context():
-            note = Note(data='test', date=17 / 5 / 2021, user_id=1)
+            note = Note(data='test')
             
             db.session.query(Note).filter_by(data="test").first()
             
             
-            # db.session.add(note)
-            # db.session.commit()
+            db.session.add(note)
+            db.session.commit()
+            
+            assert note in db.session
             
             # db.session.delete(note)
             # db.session.commit()
             
     def test_work(self):
         with self.app_context():
-            work = Work(title='test', description='working', date=17 / 5 / 2021, user_id=2, status='online', points=100)
+            work = Work(title='test', description='working', status='online', points=100)
             
             db.session.query(Work).filter_by(title='test').first()
             
+            db.session.add(work)
+            db.session.commit()
+            
+            assert work in db.session
     
     def test_user(self):
         with self.app_context():
-            user = User(email='test@test.com', password='test', first_name='tester', team_id=3, team_leader=True, points=100)
+            user = User(email='test@test.com', password='test', first_name='tester', team_leader=True, points=100)
             
             db.session.query(User).filter_by(email='test@test.com').first()
+            
+            db.session.add(user)
+            db.session.commit()
+            
+            assert user in db.session
     
     def test_team(self):
         with self.app_context():
             team = Team(name='tester')
             
             db.session.query(Team).filter_by(name='tester').first()
+            
+            db.session.add(team)
+            db.session.commit()
+            
+            assert team in db.session
