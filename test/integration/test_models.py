957d7c78fd7998d35eaaf95c83031889866c62f1
allyboy08 <alexsassman65@gmail.com>
2021-05-24 15:42:45 +0200

    update

--- test/integration/test_models.py
+++ test/integration/test_models.py
@@ -6,50 +6,79 @@ from website.__init__ import db
 
 
 class TestModels(BaseTest):
-    def test_crud(self):
+    def test_note_crud(self):
         with self.app_context():
             note = Note(data='test')
             
-            db.session.query(Note).filter_by(data="test").first()
-            
+            result = db.session.query(Note).filter_by(data="test").first()
+            self.assertIsNone(result)
             
             db.session.add(note)
             db.session.commit()
             
-            assert note in db.session
+            result = db.session.query(Note).filter_by(data="test").first()
+            self.assertIsNotNone(result)
+            # assert note in db.session
             
-            # db.session.delete(note)
-            # db.session.commit()
+            db.session.delete(note)
+            db.session.commit()
+            
+            result = db.session.query(Note).filter_by(data="test").first()
+            self.assertIsNone(result)
             
     def test_work(self):
         with self.app_context():
             work = Work(title='test', description='working', status='online', points=100)
             
-            db.session.query(Work).filter_by(title='test').first()
+            result = db.session.query(Work).filter_by(title='test').first()
+            self.assertIsNone(result)
             
             db.session.add(work)
             db.session.commit()
             
-            assert work in db.session
+            result = db.session.query(Work).filter_by(title="test").first()
+            self.assertIsNotNone(result)
+            
+            db.session.delete(work)
+            db.session.commit()
+            
+            result = db.session.query(Work).filter_by(title="test").first()
+            self.assertIsNone(result)
     
     def test_user(self):
         with self.app_context():
             user = User(email='test@test.com', password='test', first_name='tester', team_leader=True, points=100)
             
-            db.session.query(User).filter_by(email='test@test.com').first()
+            result = db.session.query(User).filter_by(email='test@test.com').first()
+            self.assertIsNone(result)
             
             db.session.add(user)
             db.session.commit()
             
-            assert user in db.session
+            result = db.session.query(User).filter_by(email='test@test.com').first()
+            self.assertIsNotNone(result)
+            
+            db.session.delete(user)
+            db.session.commit()
+            
+            result = db.session.query(User).filter_by(email="test@test.com").first()
+            self.assertIsNone(result)
     
     def test_team(self):
         with self.app_context():
             team = Team(name='tester')
             
-            db.session.query(Team).filter_by(name='tester').first()
+            result = db.session.query(Team).filter_by(name='tester').first()
+            self.assertIsNone(result)
             
             db.session.add(team)
             db.session.commit()
             
-            assert team in db.session
+            result = db.session.query(Team).filter_by(name='tester').first()
+            self.assertIsNotNone(result)
+            
+            db.session.delete(team)
+            db.session.commit()
+            
+            result = db.session.query(Team).filter_by(name="tester").first()
+            self.assertIsNone(result)
