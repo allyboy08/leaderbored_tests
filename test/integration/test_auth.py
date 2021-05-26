45e4a22088ce9517970ea46d7b3f38996eeb7515
allyboy08 <alexsassman65@gmail.com>
2021-05-26 15:56:45 +0200

    update

--- test/integration/test_auth.py
+++ test/integration/test_auth.py
@@ -4,6 +4,7 @@ sys.path.append("website\__init__")
 from website.__init__ import db
 from website.models import User
 from flask_login import current_user
+from flask import request
 
 
 class TestSignUp(BaseTest):
@@ -38,11 +39,57 @@ class TestSignUp(BaseTest):
 class TestLogin(BaseTest):
     def test_login_post_success(self):
         with self.app:
-            response = self.app.post('/log-in', data=dict(email='tests@gmail.com', password='01234567'), follow_redirects=True)
-            user = db.session.query(User).filter_by(email='tests@gmail.com').first()
+            response = self.app.post('/sign-up', data=dict(email='jeff@gmail.com', firstName='steve', password1= '01234567', password2= '01234567'), follow_redirects=True)
+            user = db.session.query(User).filter_by(email='jeff@gmail.com').first()
+            self.assertTrue(user)
+            
+            response = self.app.post('/log-in', data=dict(email='jeff@gmail.com', password='01234567'), follow_redirects=True)
+            user = db.session.query(User).filter_by(email='jeff@gmail.com').first()
             self.assertTrue(user)
             self.assertIn(b'Logged in successfully', response.data)
-            self.assertEqual(current_user.get_id(), '1')
-            self.assertIn(b'Notes', response.data)
-             
+            
+            
+            # self.assertIn(b'Notes', response.data)
+    
+    def test_login_invalid_user(self):
+        with self.app:
+            response = self.app.post('/log-in', data=dict(email='jefff@gmail.com', password='01234567'), follow_redirects=True)
+            # user = db.session.query(User).filter_by(email='jefff@gmail.com').first()
+            # self.assertTrue(user)
+            self.assertIn(b'Email does not exist', response.data)
+            
+    def test_login_wrong_password(self):
+        with self.app:
+            response = self.app.post('/sign-up', data=dict(email='jeffs@gmail.com', firstName='steves', password1= '1234567', password2= '1234567'), follow_redirects=True)
+            user = db.session.query(User).filter_by(email='jeffs@gmail.com').first()
+            self.assertTrue(user)
+            
+            
+            response = self.app.post('/log-in', data=dict(email='jeffs@gmail.com', password='0123456'), follow_redirects=True)
+            user = db.session.query(User).filter_by(email='jeffs@gmail.com').first()
+            self.assertTrue(user)
+            self.assertIn(b'Incorrect password, try again.', response.data)
+    
+    
+class TestLogout(BaseTest):
+    
+    def test_logout_route_while_logged_in(self):
+        with self.app:
+            # sign up
+            response = self.app.post('/sign-up', data=dict(email='solo@gmail.com', firstName='han', password1= 'qwerty123', password2= 'qwerty123'), follow_redirects=True)
+            user = db.session.query(User).filter_by(email='solo@gmail.com').first()
+            self.assertTrue(user)
+            # log in
+            response = self.app.post('/log-in', data=dict(email='solo@gmail.com', password='qwerty123'), follow_redirects=True)
+            user = db.session.query(User).filter_by(email='solo@gmail.com').first()
+            self.assertTrue(user)
+            
+            response = self.app.get('/log-out', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+    
+    
+
+            self.assertIn('/log-in', request.url)
+            # after log out test current user is none
+            self.assertFalse(current_user.is_active)
              
--- test/unit/test_auth.py
+++ test/unit/test_auth.py
@@ -8,19 +8,11 @@ from test.test_base import BaseTest
 from flask import request
 from flask_login import current_user, AnonymousUserMixin
 
-# class TestAuth(TestCase):
-    
-#     def test_log_in(self):
-#         with app.test_client() as client:
-#             response = client.get('/log-in')
-#             self.assertEqual(response.status_code, 200)
+
 
 class TestSignUp(BaseTest):
     def test_get_sign_up(self):
         with self.app:
-            
-            
-            
             response = self.app.get('/sign-up', follow_redirects=True)
             self.assertIn('/sign-up', request.url)
             self.assertEqual(response.status_code, 200)
@@ -54,8 +46,45 @@ class TestSignUp(BaseTest):
             self.assertFalse(user)
             self.assertIsNone(current_user.get_id())
             
-# class TestLogin(BaseTest):
-#     def test_get_login
+class TestLogin(BaseTest):
+    def test_login_page_loads(self):
+        with self.app:
+            response = self.app.get('/log-in', follow_redirects=True)
+            self.assertIn('/log-in', request.url)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Log in', response.data)
+            
+    def test_login_with_correct_data(self):
+        with self.app:
+            response = self.app.post('/sign-up', data=dict(email='test@gmail.com', firstName='mark', password1= '1234567', password2= '1234567'), follow_redirects=True)
+            
+            user = db.session.query(User).filter_by(email='test@gmail.com').first()
+            self.assertTrue(user)
+            
+            response = self.app.post('/log-in', data=dict(email='test@gmail.com', password='1234567'), follow_redirects=True)
+            user = db.session.query(User).filter_by(email='test@gmail.com').first()
+            self.assertTrue(user)
+            
+            self.assertIn(b'Logged in successfully', response.data)
+            self.assertEqual(response.status_code, 200)
+            
+class TestLogout(BaseTest):
+    def test_logout_without_being_logged_in(self):
+        with self.app:
+            # self.app.post('/log-out', data=dict(email='test@gmail.com', password='1234567'), follow_redirects=True)
+            response = self.app.get('/log-out', follow_redirects=False)
+            self.assertEqual(response.status_code, 302)
+            
+    def test_logout_route_while_logged_in(self):
+        with self.app:
+            # sign up
+            # log in
+            response = self.app.get('/logout', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+    
+    
 
-
+            self.assertIn('/log-in', request.url)
+            # after log out test current user is none
+            self.assertFalse(current_user.is_active)
 
